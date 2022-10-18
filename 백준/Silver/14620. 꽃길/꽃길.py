3376a548fbcc450cb206f838833fa7ae85386bb6
KooooY <109272632+KooooY@users.noreply.github.com>
2022-10-19 02:23:54 +0900

    [Silver II] Title: 꽃길, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -9,24 +9,22 @@ price_list = []
 for r in range(N):
     for c in range(N):
         if r == 0 or r == N - 1 or c == 0 or c == N - 1:
-            temp = 1000
+            temp = 1001
         else:
             temp = price_info[r][c] + price_info[r - 1][c] + price_info[r + 1][c] + price_info[r][c - 1] + price_info[r][c + 1]
         price_list.append(temp)
 
-price_list.extend([1000]*N)
-
 ans = 99999999
 for i in range(N+1, N**2-N):
     visited_i = {i+N, i+N-1, i+N, i+N+1, i+2*N}
-    if price_list[i] == 200:
+    if price_list[i] == 1001:
         continue
     for j in range(i+3, N**2-N):
-        if j in visited_i or price_list[j] == 200:
+        if j in visited_i or price_list[j] == 1001:
             continue
         visited_j = {j+N, j+N-1, j+N, j+N+1, j+2*N}
         for k in range(j+3, N**2-N):
-            if k in (visited_i|visited_j) or price_list[k] == 200:
+            if k in (visited_i|visited_j) or price_list[k] == 1001:
                 continue
             if ans > price_list[i]+price_list[j]+price_list[k]:
                 ans = price_list[i]+price_list[j]+price_list[k]
