1b8b357d2810ec07d0761881df9e41fdf77b1a03
KooooY <109272632+KooooY@users.noreply.github.com>
2022-12-10 03:40:44 +0900

    [Gold III] Title: 음악프로그램, Time: 304 ms, Memory: 30680 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -0,0 +1,33 @@
+N, M = map(int, input().split())
+adj_dict = {i:[] for i in range(1, N+1)}
+singer = [s for s in range(1, N+1)]
+ans = []
+
+for _ in range(M):
+    temp = list(map(int, input().split()))
+    for i in range(1, temp[0]):
+        adj_dict[temp[i+1]].extend(temp[1:i+1])
+
+Q = []
+
+for j in adj_dict:
+    if not adj_dict[j]:
+        Q.append(j)
+
+while Q:
+    item = Q.pop(0)
+    ans.append(item)
+    singer.remove(item)
+
+    for k in singer:
+        if item in adj_dict[k]:
+            while item in adj_dict[k]:
+                adj_dict[k].remove(item)
+            if not adj_dict[k]:
+                Q.append(k)
+
+if singer:
+    print(0)
+else:
+    for singer in ans:
+        print(singer)
