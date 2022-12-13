14d145af1b4796f86cdfcb9e9c0074e21b85362d
KooooY <109272632+KooooY@users.noreply.github.com>
2022-12-14 05:32:41 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,7 +3,8 @@ time_info = list(map(int, input().split()))
 ans = 0
 
 time_info.sort()
-for i in range(N):
-    ans += sum(time_info[:i+1])
+sum_list = [time_info[0]]
+for i in range(1,N):
+    sum_list.append(sum_list[i-1]+time_info[i])
 
-print(ans)
+print(sum(sum_list))
