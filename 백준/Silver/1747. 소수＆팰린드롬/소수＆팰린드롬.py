d5a0cffbdae4a9d45c96029bf07e8f3ef21d8285
KooooY <109272632+KooooY@users.noreply.github.com>
2023-12-09 01:00:36 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 620 ms, Memory: 111332 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,18 +1,15 @@
 N = int(input())
 
-arr = [i for i in range(10000001)]
-answer = 0
+arr = [i for i in range(2000001)]
 arr[1] = 0
 
-for j in range(2, int(10000000**(1/2))+1):
+for j in range(2, int(2000000**(1/2))+1):
     if not arr[j]:
         continue
-    for k in range(j*j, 10000001, j):
+    for k in range(j*j, 2000001, j):
         arr[k] = 0
 
-for l in range(N, 10000001):
+for l in range(N, 2000001):
     if arr[l] and str(arr[l]) == str(arr[l])[::-1]:
-        answer = arr[l]
+        print(arr[l])
         break
-
-print(answer)
