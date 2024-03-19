8e1b19358e3f20066b3881e790fe82447decbef2
KooooY <109272632+KooooY@users.noreply.github.com>
2024-03-19 23:28:06 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4532 ms, Memory: 59064 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,25 @@
+import heapq
+
+V, E = map(int, input().split())
+matrix = [[] for _ in range(V)]
+mst = set()
+heap = []
+answer = 0
+
+for _ in range(E):
+    A, B, C = map(int, input().split())
+    matrix[A - 1].append((C, B - 1))
+    matrix[B - 1].append((C, A - 1))
+
+heapq.heappush(heap, (0, 1))
+
+while heap:
+    cur = heapq.heappop(heap)
+    if cur[1] not in mst:
+        mst.add(cur[1])
+        answer += cur[0]
+        for i in matrix[cur[1]]:
+            if i[1] not in mst:
+                heapq.heappush(heap, i)
+
+print(answer)
