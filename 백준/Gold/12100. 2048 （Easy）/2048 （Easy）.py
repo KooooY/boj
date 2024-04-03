3717b415f145af8cc75367917c895eb1d6fcb786
KooooY <109272632+KooooY@users.noreply.github.com>
2024-04-04 03:19:42 +0900

    [Gold II] Title: 2048 (Easy), Time: 188 ms, Memory: 34412 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,51 +1,162 @@
-def push(direction):
-    x, y = direction
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-    a = int((N - 1) / 2 + (x + y) * (1 - N) / 2)
-    b = int((N - 1) / 2 + (x + y) * (N + 1) / 2)
-    s = x + y
+def moveup(cur, count):
+    global answer
+    temp = [[0] * N for _ in range(N)]
+    flag = [0] * N
+    for i in range(N):
+        new = deque()
+        for j in range(N):
+            if cur[j][i]:
+                if cur[j][i] > answer:
+                    answer = cur[j][i]
+                if new:
+                    if new[-1] == cur[j][i]:
+                        new[-1] = str(cur[j][i] * 2)
+                        if cur[j][i] * 2 > answer:
+                            answer = cur[j][i] * 2
+                        continue
+                new.append(cur[j][i])
+        idx = 0
 
-    for rc in range(a, b, s):
-        temp = []
-        for cr in range(a, b, s):
-            rorc = rc * abs(y) + cr * abs(x)
-            corr = cr * abs(y) + rc * abs(x)
-            if board_copy[rorc][corr]:
-                temp.append(board_copy[rorc][corr])
-                board_copy[rorc][corr] = 0
+        for k in new:
+            if type(k) == int:
+                if idx:
+                    if temp[idx - 1][i] == k and not flag[idx - 1]:
+                        temp[idx - 1][i] *= 2
+                        flag[idx - 1] = 1
+                        continue
+            else:
+                flag[idx] = 1
+            temp[idx][i] = int(k)
+            idx += 1
+    if count == 5:
+        return
+    else:
+        count += 1
+        moveleft(temp, count)
+        moveright(temp, count)
+        movedown(temp, count)
+        moveup(temp, count)
 
-        c_i = int((N - 1) / 2 + (x + y) * (N - 1) / 2)
+def movedown(cur, count):
+    global answer
+    temp = [[0] * N for _ in range(N)]
+    flag = [0] * N
+    for i in range(N):
+        new = deque()
+        for j in range(N - 1, -1, -1):
+            if cur[j][i]:
+                if new:
+                    if new[-1] == cur[j][i]:
+                        new[-1] = str(cur[j][i] * 2)
+                        if cur[j][i] * 2 > answer:
+                            answer = cur[j][i] * 2
+                        continue
+                new.append(cur[j][i])
+        idx = N - 1
+        for k in new:
+            if type(k) == int:
+                if idx < N - 1:
+                    if temp[idx + 1][i] == k and not flag[idx + 1]:
+                        temp[idx + 1][i] *= 2
+                        flag[idx + 1] = 1
+                        continue
+            else:
+                flag[idx] = 1
 
-        while temp:
-            num = temp.pop()
-            if len(temp) > 0 and num == temp[-1]:
-                num += temp.pop()
-            board_copy[rc * abs(y) + c_i * abs(x)][c_i * abs(y) + rc * abs(x)] = num
-            c_i -= x + y
+            temp[idx][i] = int(k)
+            idx -= 1
+    if count == 5:
+        return
+    else:
+        count += 1
+        moveleft(temp, count)
+        moveright(temp, count)
+        moveup(temp, count)
+        movedown(temp, count)
 
+def moveleft(cur, count):
+    global answer
+    temp = [[0] * N for _ in range(N)]
+    flag = [0] * N
+    for i in range(N):
+        new = deque()
+        for j in range(N):
+            if cur[i][j]:
+                if new:
+                    if new[-1] == cur[i][j]:
+                        new[-1] = str(cur[i][j] * 2)
+                        if cur[i][j] * 2 > answer:
+                            answer = cur[i][j] * 2
+                        continue
+                new.append(cur[i][j])
+        idx = 0
+        for k in new:
+            if type(k) == int:
+                if idx:
+                    if temp[i][idx - 1] == k and not flag[idx - 1]:
+                        temp[i][idx - 1] *= 2
+                        flag[idx - 1] = 1
+                        continue
+            else:
+                flag[idx] = 1
+            temp[i][idx] = int(k)
+            idx += 1
+    if count == 5:
+        return
+    else:
+        count += 1
+        moveup(temp, count)
+        movedown(temp, count)
+        moveright(temp, count)
+        moveleft(temp, count)
+
+def moveright(cur, count):
+    global answer
+    temp = [[0] * N for _ in range(N)]
+    flag = [0] * N
+    for i in range(N):
+        new = deque()
+        for j in range(N - 1, -1, -1):
+            if cur[i][j]:
+                if new:
+                    if new[-1] == cur[i][j]:
+                        new[-1] = str(cur[i][j] * 2)
+                        if cur[i][j] * 2 > answer:
+                            answer = cur[i][j] * 2
+                        continue
+                new.append(cur[i][j])
+        idx = N - 1
+        for k in new:
+            if type(k) == int:
+                if idx < N - 1:
+                    if temp[i][idx + 1] == k and not flag[idx + 1]:
+                        temp[i][idx + 1] *= 2
+                        flag[idx + 1] = 1
+                        continue
+            else:
+                flag[idx] = 1
+            temp[i][idx] = int(k)
+            idx -= 1
+    if count == 5:
+        return
+    else:
+        count += 1
+        moveup(temp, count)
+        movedown(temp, count)
+        moveleft(temp, count)
+        moveright(temp, count)
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-max_num = 0
-directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+answer = 0
 
-for i in range(1 << 10):
-    cmd_list = []
-    for j in range(0, 10, 2):
-        if i & (1 << j) and i & (1 << (j + 1)):
-            cmd_list.append(directions[0])
-        elif i & (1 << j):
-            cmd_list.append(directions[1])
-        elif i & (1 << (j + 1)):
-            cmd_list.append(directions[2])
-        else:
-            cmd_list.append(directions[3])
+moveup(board, 1)
+movedown(board, 1)
+moveleft(board, 1)
+moveright(board, 1)
 
-    board_copy = [board[i][:] for i in range(N)]
-    for cmd in cmd_list:
-        push(cmd)
-    now_max_num = max(map(max, board_copy))
-    if max_num < now_max_num:
-        max_num = now_max_num
-
-print(max_num)
+print(answer)
