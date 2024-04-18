d59ee8523df49dbf41979aec6c63e033d7a3e0e3
KooooY <109272632+KooooY@users.noreply.github.com>
2024-04-18 16:28:44 +0900

    [Gold I] Title: 계단 수, Time: 624 ms, Memory: 39616 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -1,21 +1,3 @@
-def floor_num(pre_num, pre_binary, turn):
-    global answer
-    if pre_binary == 1023:
-        answer += 1
-    if turn == N:
-        return
-
-    cur_min = pre_num % 10 - 1
-    cur_max = pre_num % 10 + 1
-    if pre_num % 10 > 0:
-        new_binary = pre_binary | (1 << cur_min)
-        floor_num(pre_num * 10 + cur_min, new_binary, turn + 1)
-    if pre_num % 10 < 9:
-        new_binary = pre_binary | (1 << cur_max)
-        floor_num(pre_num * 10 + cur_max, new_binary, turn + 1)
-    return
-
-
 N = int(input())
 dp = [[[0] * (2 ** 10) for _ in range(10)] for _ in range(N + 1)]
 answer = 0
