96cca381e391d270eab472d30c3575866149a8b2
KooooY <109272632+KooooY@users.noreply.github.com>
2024-04-23 21:32:05 +0900

    [Gold IV] Title: 불!, Time: 3380 ms, Memory: 170672 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -22,27 +22,25 @@ for i in range(R):
     maze.append(list(temp))
 
 Q = deque()
-Q.append(jihoon)
 Q.extend(fires)
-jihoon_visited = {(jihoon[0], jihoon[1])}
+Q.append(jihoon)
+fires.add((jihoon[0], jihoon[1]))
 
-while Q and answer == 'IMPOSSIBLE':
+while Q:
     cur = Q.popleft()
     if len(cur) == 3:
         cur_r, cur_c, dist = cur
-        if (cur_r, cur_c) in fires:
-            continue
         if not cur_r or cur_r == R - 1 or not cur_c or cur_c == C - 1:
             answer = dist + 1
             break
         for l in range(4):
-            if 0 <= cur_r + dr[l] < R and 0 <= cur_c + dc[l] < C and (cur_r + dr[l], cur_c + dc[l]) not in jihoon_visited and (cur[0] + dr[l], cur[1] + dc[l]) not in fires and maze[cur_r + dr[l]][cur_c + dc[l]] == '.':
-                jihoon_visited.add((cur_r + dr[l], cur_c + dc[l]))
+            if 0 <= cur_r + dr[l] < R and 0 <= cur_c + dc[l] < C and (cur[0] + dr[l], cur[1] + dc[l]) not in fires and maze[cur_r + dr[l]][cur_c + dc[l]] == '.':
+                fires.add((cur_r + dr[l], cur_c + dc[l]))
                 Q.append((cur_r + dr[l], cur_c + dc[l], dist + 1))
 
     else:
         for k in range(4):
-            if 0 <= cur[0] + dr[k] < R and 0 <= cur[1] + dc[k] < C and (cur[0] + dr[k], cur[1] + dc[k]) not in fires and (maze[cur[0] + dr[k]][cur[1] + dc[k]] == '.' or maze[cur[0] + dr[k]][cur[1] + dc[k]] == 'J'):
+            if 0 <= cur[0] + dr[k] < R and 0 <= cur[1] + dc[k] < C and (cur[0] + dr[k], cur[1] + dc[k]) not in fires and maze[cur[0] + dr[k]][cur[1] + dc[k]] != '#':
                 fires.add((cur[0] + dr[k], cur[1] + dc[k]))
                 Q.append((cur[0] + dr[k], cur[1] + dc[k]))
 
