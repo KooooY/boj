a2d923ef336aa0bc14eb4bc4024e5a55b78b1fc1
KooooY <109272632+KooooY@users.noreply.github.com>
2024-04-24 23:18:29 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -2,20 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-skyline = [(0, 0)] + [tuple(map(int, input().split())) for _ in range(n)] + [(0, 0)]
 answer = 0
 stack = []
+prev_h = 0
 
-for i in range(1, n + 2):
-    if skyline[i][1] < skyline[i - 1][1]:
+for _ in range(n):
+    x, y = map(int, input().split())
+    if y < prev_h:
         while stack:
             cur = stack.pop()
-            if cur > skyline[i][1]:
+            if cur > y:
                 answer += 1
             else:
-                if cur < skyline[i][1]:
+                if cur < y:
                     stack.append(cur)
                 break
-    stack.append(skyline[i][1])
+    stack.append(y)
+    prev_h = y
+
+while stack:
+    cur = stack.pop()
+    if cur:
+        answer += 1
 
 print(answer)
