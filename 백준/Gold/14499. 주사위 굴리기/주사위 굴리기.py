4b0ba6fedb6776fb6d198bc5f0f6bbc59735626c
KooooY <109272632+KooooY@users.noreply.github.com>
2024-05-03 00:23:47 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,60 +1,29 @@
-M, N, R, C, K = map(int, input().split())
-mm = []
-for i in range(M):
-    mm += [list(map(int, input().split()))]
+import sys
+input = sys.stdin.readline
 
-cmds = list(map(int, input().split()))
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-from collections import deque
-dice = [-1,0,0,0,0,0,0]
-direc = {4:[1,0], 3:[-1,0], 2:[0,-1],1:[0,1]}
-for c in cmds:
-    tr = R + direc[c][0]
-    tc = C + direc[c][1]
-    if tr < 0 or tc < 0 or tr >= M or tc >= N:
-        continue
-    R = tr
-    C = tc
-    if c == 4: ## South
-        st = deque([dice[5]])
-        dice[5] = dice[1]
-        st += [dice[6]]
-        dice[6] = st.popleft()
-        st += [dice[2]]
-        dice[2] = st.popleft()
-        st += [dice[1]]
-        dice[1] = st.popleft()
-    elif c == 3: ## North
-        st = deque([dice[1]])
-        dice[1] = dice[5]
-        st += [dice[2]]
-        dice[2] = st.popleft()
-        st += [dice[6]]
-        dice[6] = st.popleft()
-        st += [dice[5]]
-        dice[5] = st.popleft()
-    elif c == 2: ## West
-        q = deque([dice[4]])
-        dice[4] = dice[1]
-        q += [dice[6]]
-        dice[6] = q.popleft()
-        q += [dice[3]]
-        dice[3] = q.popleft()
-        q += [dice[1]]
-        dice[1] = q.popleft()
-    elif c == 1: ## East
-        q = deque([dice[3]])
-        dice[3] = dice[1]
-        q += [dice[6]]
-        dice[6] = q.popleft()
-        q += [dice[4]]
-        dice[4] = q.popleft()
-        q += [dice[1]]
-        dice[1] = q.popleft()
+N, M, x, y, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+moving = list(map(int, input().split()))
+dice = [0, 0, 0, 0, 0, 0]
 
-    if mm[R][C] == 0:
-        mm[R][C] = dice[6]
-    else:
-        dice[6] = mm[R][C]
-        mm[R][C] = 0
-    print(dice[1])
+for m in moving:
+    new_x, new_y = x + dx[m - 1], y + dy[m - 1]
+    if 0 <= new_x < N and 0 <= new_y < M:
+        x, y = new_x, new_y
+        if m == 1:
+            dice = [dice[3], dice[1], dice[0], dice[5], dice[4], dice[2]]
+        elif m == 2:
+            dice = [dice[2], dice[1], dice[5], dice[0], dice[4], dice[3]]
+        elif m == 3:
+            dice = [dice[4], dice[0], dice[2], dice[3], dice[5], dice[1]]
+        else:
+            dice = [dice[1], dice[5], dice[2], dice[3], dice[0], dice[4]]
+        print(dice[0])
+        if arr[x][y]:
+            dice[5] = arr[x][y]
+            arr[x][y] = 0
+        else:
+            arr[x][y] = dice[5]
