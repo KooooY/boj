dc8d964e977ad3679fa45cb18b4ec469e01fe82e
KooooY <109272632+KooooY@users.noreply.github.com>
2024-06-16 21:36:25 +0900

    [Gold III] Title: 파티, Time: 2500 ms, Memory: 34212 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,36 @@
+import heapq
+
+def dijk(s, e):
+    min_dist = [100001] * N
+    visited = {i for i in range(N)}
+    min_dist[s] = 0
+    next_cur = []
+    heapq.heappush(next_cur, (0, s))
+
+    while next_cur:
+        cur_dist, cur = heapq.heappop(next_cur)
+        if cur in visited:
+            for distance, destination in map_info[cur]:
+                if destination in visited:
+                    if min_dist[cur] + distance < min_dist[destination]:
+                        min_dist[destination] = min_dist[cur] + distance
+                        heapq.heappush(next_cur, (min_dist[destination], destination))
+            visited.remove(cur)
+
+    return min_dist[e]
+
+
+N, M, X = map(int, input().split())
+map_info = [[] for _ in range(N)]
+answer = 0
+
+for _ in range(M):
+    start, end, dist = map(int, input().split())
+    map_info[start - 1].append((dist, end - 1))
+
+for n in range(N):
+    time = dijk(n, X - 1) + dijk(X - 1, n)
+    if time > answer:
+        answer = time
+
+print(answer)
